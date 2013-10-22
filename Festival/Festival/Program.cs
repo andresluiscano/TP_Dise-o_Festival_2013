4969ff7e22bff29c7dcd1e9619768d7455b7728d
Andres Cano <andresluiscano@gmail.com>
2013-10-22 12:14:37 -0300

    Forms altas y bajas

--- Festival/Festival/Program.cs
+++ Festival/Festival/Program.cs
@@ -15,7 +15,9 @@ namespace Festival
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmLogin());
         }
+
     }
+
 }
